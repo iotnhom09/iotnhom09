aed37e9b8ae31535466e6e4f32684d0d066cc38f
iotnhom09 <tranthiyenngoc20022018@gmail.com>
2023-05-16 10:16:46 +0700

    update

--- js/custom.js
+++ js/custom.js
@@ -111,6 +111,45 @@
         $(".naccs ul").height(listItemHeight + "px");
       }
   });
+//Control fan
+const fan = document.querySelector('.blades')
+const btn = document.querySelectorAll('.btn')
+const btnTurn = document.querySelector('.btn-0')
+
+
+btn.forEach(btn => {
+    btn.addEventListener('click', ()=>{
+        let nav = btn.getAttribute('data-nav');
+        // console.log(nav)
+        if (nav == 1){
+            active(btn)
+            speed(2)
+        }
+        else if (nav == 2){
+            active(btn)
+            speed(1)
+        }
+        else if (nav == 3){
+            active(btn)
+            speed(0.5)
+        }
+        else{
+            active(btn)
+            speed(0)
+        }
+    })
+})
+
+function active(element){
+    btn.forEach(btn => {
+        btn.classList.remove('active')
+    });
+    element.classList.add('active')
+}
+
+function speed(value) {
+    fan.style.animationDuration = `${value}s`
+}
 
 
 	
